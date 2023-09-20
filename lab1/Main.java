01851924a8afb0e14b972f9c0b84f866937ced01
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-09-20 14:59:56 +0300

    Update Main.java

--- lab1/Main.java
+++ lab1/Main.java
@@ -5,15 +5,6 @@ public class Main {
         return min + (float) (Math.random() * (max - min));
     }
 
-    private static boolean checkIfValueInArray(long[] array, long target) {
-        for (long value : array) {
-            if (value == target) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static void main(String[] args) {
         // Crating necessary arrays
         long[] cArray = {3, 5, 7, 9, 11, 13, 15, 17, 19};
@@ -28,13 +19,12 @@ public class Main {
             for (int j = 0; j < 16; j++) {
                 float x = xArray[j];
 
-                if (cArray[i] == 3) {
-                    result[i][j] = cos(asin(pow(((x - 5.5) / 17), 2)));
-                } else if (checkIfValueInArray(new long[]{13, 15, 17, 19}, cArray[i])) {
-                    result[i][j] = pow(exp(pow(x, x)), (double) 1 / 3);
-                } else {
-                    result[i][j] = asin(pow(sin(exp(Math.cos(x))), 2));
-                }
+                double currentResult = switch ((int) cArray[i]) {
+                    case 3 -> cos(asin(pow(((x - 5.5) / 17), 2)));
+                    case 13, 15, 17, 19 -> pow(exp(pow(x, x)), (double) 1 / 3);
+                    default -> asin(pow(sin(exp(Math.cos(x))), 2));
+                };
+                result[i][j] = currentResult;
             }
         }
 
